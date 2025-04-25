b5cfcaa0c7135ff31ba1cd43dc63ed6bf4d0da5b
Harshdeep Singh <hs703808@gmail.com>
2025-04-26 00:29:15 +0530

    updates in ui and content

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -2,27 +2,27 @@ import FaqCard from "@/components/main/FaqCard";
 
 const faqs = [
   {
-    img: "https://cdn-icons-png.flaticon.com/128/857/857681.png",
+    img: "https://cdn-icons-png.flaticon.com/128/562/562678.png",
     imgTitle: "food",
     title: "Getting Started",
   },
   {
-    img: "https://cdn-icons-png.flaticon.com/128/857/857681.png",
+    img: "https://cdn-icons-png.flaticon.com/128/2224/2224150.png",
     imgTitle: "food",
     title: "Nutrition & Ingredients",
   },
   {
-    img: "https://cdn-icons-png.flaticon.com/128/857/857681.png",
+    img: "https://cdn-icons-png.flaticon.com/128/9582/9582476.png",
     imgTitle: "user icon",
     title: "Managing My Subscription",
   },
   {
-    img: "https://cdn-icons-png.flaticon.com/128/857/857681.png",
+    img: "https://cdn-icons-png.flaticon.com/128/679/679720.png",
     imgTitle: "packing box",
     title: "Receiving My Meals",
   },
   {
-    img: "https://cdn-icons-png.flaticon.com/128/857/857681.png",
+    img: "https://cdn-icons-png.flaticon.com/128/2771/2771460.png",
     imgTitle: "food",
     title: "Enjoying My Meals",
   },
@@ -32,7 +32,7 @@ const page = () => {
   return (
     <div className="flex flex-col gap-6 w-full h-full max-h-screen overflow-auto scrollbar-hide bg-[#F4F3EF]">
       {/* heading */}
-      <h1 className="text-black font-Arial Black text-5xl font-bold  text-center pt-8">
+      <h1 className="pt-8 text-5xl font-bold text-center text-black font-Arial Black">
         Frequently Asked Questions
       </h1>
       {/* searchbar */}
@@ -40,12 +40,12 @@ const page = () => {
         <input
           type="text"
           name="faq"
-          className="border border-gray-200 w-full p-3 bg-white text-black"
+          className="w-full p-3 text-black bg-white border border-gray-200"
           placeholder="What can we help you with?"
         />
       </div>
       {/* questions */}
-      <div className=" flex w-full h-full px-12 justify-center items-center py-2">
+      <div className="flex items-center justify-center w-full h-full px-12 py-2 ">
         <div className="lg:w-[65%] h-auto flex flex-wrap gap-6 justify-start">
           {faqs.map((data) => {
             return (
@@ -60,22 +60,22 @@ const page = () => {
         </div>
       </div>
       {/* contact us */}
-      <div className="w-full flex flex-col p-6 h-full items-center gap-4">
-        <h2 className="text-black text-3xl font-bold font-arial">
+      <div className="flex flex-col items-center w-full h-full gap-4 p-6">
+        <h2 className="text-3xl font-bold text-black font-arial">
           Can&apos;t find your answer?
         </h2>
         <button
           type="submit"
-          className="border px-6 py-3 bg-fit-red text-white rounded-lg"
+          className="px-6 py-3 text-white border rounded-lg bg-fit-red"
         >
           Contact Us
         </button>
       </div>
       {/* bg-img section */}
-      <div className="w-full relative my-12  min-h-[600px] bg-[url('/temp/temp.webp')] bg-cover bg-center flex justify-center mt-40">
+      <div className="w-full relative my-12  min-h-[600px] bg-[url('/images/food-main.avif')] bg-cover bg-center flex justify-center mt-40">
         <div className="flex flex-col items-center mx-12 px-12 absolute top-[-80px] bg-white gap-6 py-10 border-gray-200 rounded-sm">
           <h1 className="text-[#000] font-Arial Black text-4xl font-bold  text-center pt-8">
-            Get Up to $75 Off Our Ready-Made Meals
+            Get Up to 25% Off Our Ready-Made Meals
           </h1>
           <span className="text-[#4b4d4c] text-center">
             Get fully-prepared, gourmet meals delivered to your doorstep. No
@@ -83,7 +83,7 @@ const page = () => {
           </span>
           <button
             type="submit"
-            className=" border px-6 py-3 bg-fit-red text-white rounded-lg"
+            className="px-6 py-3 text-white border rounded-lg bg-fit-red"
           >
             Get Offer
           </button>
--- src/app/weekly-menu/page.tsx
+++ src/app/weekly-menu/page.tsx
@@ -1,100 +1,168 @@
-import GetBtn from '@/components/buttons/GetBtn'
-import MealCard from '@/components/main/MealCard'
+import GetBtn from "@/components/buttons/GetBtn";
+import MealCard from "@/components/main/MealCard";
 // import Image from 'next/image'
-import React from 'react'
+import React from "react";
+
+const meals = [
+  {
+    image: "/images/indian-thali.avif",
+    title: "🍛 Indian Thali",
+    desc: "with Dal, Rice, Roti & Mixed Veggies",
+  },
+  {
+    image: "/images/chicken-skewers.avif",
+    title: "🍗 Chicken Skewers",
+    desc: "with Mint Chutney & Salad",
+  },
+  {
+    image: "/images/rajma-rice.avif",
+    title: "🍲 Rajma Chawal",
+    desc: "Kidney Beans Curry with Steamed Rice",
+  },
+  {
+    image: "/images/paneer-masala.avif",
+    title: "🧀 Paneer Butter Masala",
+    desc: "with Jeera Rice & Naan",
+  },
+  {
+    image: "/images/biryani.avif",
+    title: "🥘Chicken Biryani",
+    desc: "Served with Raita",
+  },
+  {
+    image: "/images/aloo-paratha.avif",
+    title: "🌯 Aloo Paratha",
+    desc: "with Pickle & Curd",
+  },
+];
+
+const addOnes = [
+  {
+    image: "/images/mango-lassi.avif",
+    title: "🥭 Mango Lassi",
+    desc: "A refreshing yogurt-based drink",
+  },
+  {
+    image: "/images/cup-chai.avif",
+    title: "🍵 Masala Chai",
+    desc: "Traditional spiced Indian tea",
+  },
+  {
+    image: "/images/samosa.avif",
+    title: "🥟 Samosa",
+    desc: "Crispy pastry filled with spiced potatoes",
+  },
+  {
+    image: "/images/gulab-jamun.avif",
+    title: "🍬 Gulab Jamun",
+    desc: "Soft milk-solid sweet soaked in rose syrup",
+  },
+  {
+    image: "/images/coconut-barfi.avif",
+    title: "🥥 Coconut Barfi",
+    desc: "A sweet treat made with coconut & milk",
+  },
+  
+];
 
 const Page = () => {
   return (
     <div>
-      <div className="w-full flex justify-center items-center gap-2">
-
-        <button className=' w-fit cursor-pointer p-2 rounded-lg hover:bg-fit-red hover:text-white' >
-          <h5 className='capitalize'>apr</h5>
+      <div className="flex items-center justify-center w-full gap-2">
+        <button className="p-2 rounded-lg cursor-pointer w-fit hover:bg-fit-red hover:text-white">
+          <h5 className="capitalize">apr</h5>
           <h5>19-25</h5>
         </button>
 
-        <button className=' w-fit cursor-pointer p-2 rounded-lg hover:bg-fit-red hover:text-white' >
-          <h5 className='capitalize'>apr</h5>
+        <button className="p-2 rounded-lg cursor-pointer w-fit hover:bg-fit-red hover:text-white">
+          <h5 className="capitalize">apr</h5>
           <h5>19-25</h5>
         </button>
 
-        <button className=' w-fit cursor-pointer p-2 rounded-lg hover:bg-fit-red hover:text-white' >
-          <h5 className='capitalize'>apr</h5>
+        <button className="p-2 rounded-lg cursor-pointer w-fit hover:bg-fit-red hover:text-white">
+          <h5 className="capitalize">apr</h5>
           <h5>19-25</h5>
         </button>
-
       </div>
 
       <div className="w-full h-full pb-10 bg-[#F1F1EA] flex flex-col gap-4">
-
-        <div className="w-full flex flex-col gap-4">
+        <div className="flex flex-col w-full gap-4">
           <div className="w-full flex flex-col justify-center items-center gap-3 h-[138px] px-4 text-center">
-            <h2 className='sm:text-4xl text-3xl font-medium sm:font-semibold text-[#4b4d4c] '>Explore our Flexible Weekly Menu</h2>
-            <h4 className='sm:text-base text-sm text-[#4b4d4c]'>Browse the 30+ gourmet meals featured on this week&apos;s menu</h4>
+            <h2 className="sm:text-4xl text-3xl font-medium sm:font-semibold text-[#4b4d4c] ">
+              Explore our Flexible Weekly Menu
+            </h2>
+            <h4 className="sm:text-base text-sm text-[#4b4d4c]">
+              Browse the 30+ gourmet meals featured on this week&apos;s menu
+            </h4>
           </div>
 
           {/* // Ready Meals  */}
-          <div className="w-full px-4 flex flex-col gap-4 ">
-
-            <div className="w-fit flex flex-col gap-4 mx-3 md:mx-10">
-              <h2 className='text-2xl font-semibold '>Ready-made meals</h2>
-              <h4>Fresh meals that arrive ready to heat, eat and enjoy in minutes.</h4>
+          <div className="flex flex-col w-full gap-4 px-4 ">
+            <div className="flex flex-col gap-4 mx-3 w-fit md:mx-10">
+              <h2 className="text-2xl font-semibold ">Ready-made meals</h2>
+              <h4>
+                Fresh meals that arrive ready to heat, eat and enjoy in minutes.
+              </h4>
             </div>
 
-            <div className="w-full h-full flex gap-3 sm:gap-5 xl:gap-8 flex-wrap justify-center items-center">
+            <div className="flex flex-wrap items-center justify-center w-full h-full gap-3 sm:gap-5 xl:gap-8">
               {/* // Cards  */}
-              {Array(6).fill(0).map((e, i) => {
-                console.log(e)
-                return (
-                  <MealCard key={i} image='/temp/temp.webp' title='🍕Shredded Jerk Chicken & Rice' desc='with Sweet Potatoes & Mango Jam' />
-                )
-              })}
+              {meals.map((meal, i) => (
+                <MealCard
+                  key={i}
+                  image={meal.image}
+                  title={meal.title}
+                  desc={meal.desc}
+                />
+              ))}
             </div>
 
-            <button className='px-4 py-2 border border-fit-red w-fit  rounded-lg mx-auto font-semibold text-base text-fit-red hover:bg-fit-red hover:text-white'>Load more meals</button>
-
+            <button className="px-4 py-2 mx-auto text-base font-semibold border rounded-lg border-fit-red w-fit text-fit-red hover:bg-fit-red hover:text-white">
+              Load more meals
+            </button>
           </div>
         </div>
 
-        <div className="w-full flex flex-col gap-4">
+        <div className="flex flex-col w-full gap-4">
           <div className="w-fit flex flex-col gap-2 px-4 h-[70px] mx-3 md:mx-10 ">
-            <h2 className='sm:text-xl text-lg font-semibold '>
-            Add-ons
-              </h2>
-            <h4 className='sm:text-base text-sm '>
-            You’re not limited to just the meals, you can add other goodies to your meals!
-              </h4>
+            <h2 className="text-lg font-semibold sm:text-xl ">Add-ons</h2>
+            <h4 className="text-sm sm:text-base ">
+              You’re not limited to just the meals, you can add other goodies to
+              your meals!
+            </h4>
           </div>
 
           {/* // Ready Meals  */}
-          <div className="w-full px-4 flex flex-col gap-4 ">
-
-            <div className="w-full h-full flex gap-3 sm:gap-5 xl:gap-8 flex-wrap justify-center items-center">
+          <div className="flex flex-col w-full gap-4 px-4 ">
+            <div className="flex flex-wrap items-center justify-center w-full h-full gap-3 sm:gap-5 xl:gap-8">
               {/* // Cards  */}
-              {Array(6).fill(0).map((e, i) => {
-                console.log(e)
-                return (
-                  <MealCard key={i} image='/temp/temp.webp' title='🍕Shredded Jerk Chicken & Rice' desc='with Sweet Potatoes & Mango Jam' />
-                )
-              })}
+              {addOnes.map((item, i) => (
+                <MealCard
+                  key={i}
+                  image={item.image}
+                  title={item.title}
+                  desc={item.desc}
+                />
+              ))}
             </div>
 
-            <button className='px-4 py-2 border border-fit-red w-fit  rounded-lg mx-auto font-semibold text-base text-fit-red hover:bg-fit-red hover:text-white'>Load more meals</button>
-
+            <button className="px-4 py-2 mx-auto text-base font-semibold border rounded-lg border-fit-red w-fit text-fit-red hover:bg-fit-red hover:text-white">
+              Load more meals
+            </button>
           </div>
         </div>
 
         <div className="w-full h-full flex justify-center py-4 pt-5 sticky bottom-0 bg-[#F1F1EA]">
-          <GetBtn  link='/our-plans' content='Get started'/>
+          <GetBtn link="/our-plans" content="Get started" />
         </div>
 
-        <h4 className='text-center px-4 text-base '>Enjoy flavourful meals with no fuss. Pick your dishes, skip weeks, cancel anytime.</h4>
+        <h4 className="px-4 text-base text-center ">
+          Enjoy flavourful meals with no fuss. Pick your dishes, skip weeks,
+          cancel anytime.
+        </h4>
       </div>
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
